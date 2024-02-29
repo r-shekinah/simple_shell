f393d669f11bdc4dee623c3c5def93f52825ae09
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-29 04:22:39 -0800

    did a few debugging

--- spiral.c
+++ spiral.c
@@ -6,27 +6,41 @@
 int main(void)
 {
 	char *buffer = NULL;
-	size_t count = 0;
-	int status;
+	int status, index, input_status, path_status, child_status, there_was_space;
+	pid_t child_process;
+	size_t count;
 
-	printf("$ ");
+	there_was_space = 0;
 
+	input_status = isatty(0);
+	if (input_status == 1)
+		printf("$ ");
 	while ((status = getline(&buffer, &count, stdin)) != EOF)
 	{
-		strtok(buffer, "\n");
-		if (buffer[0] == '/')
+		if (input_status == 0)
 		{
-			path_handle(buffer);
+			non_stdin_handle(buffer);
+			return (0);
 		}
-		else
+		for (index = 0; *(buffer + index) != '\n'; index++)
 		{
-			command_handle(buffer);
+			if (*(buffer + index) == ' ' || *(buffer + index) == '\n')
+				there_was_space = 1;
+			else
+				there_was_space = 0;
+			if (there_was_space == 0)
+			{
+				strtok(buffer, "\n");
+				if (*(buffer + index) == '/')
+					path_handle(buffer);
+				else
+					command_handle(buffer);
+				break;
+			}
 		}
 		printf("$ ");
 	}
-	if (status)
-	{
+	if (status == EOF)
 		free(buffer);
-	}
 	return (0);
 }
