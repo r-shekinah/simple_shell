72c3ff34d20f68ac437522c441683b81f1a4b2e5
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-04 01:48:02 -0800

    add the exit and replaced all breaks in spiral to exit, might change the CTRL + D to EXIT_FAILURE

--- spiral.c
+++ spiral.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	char *buffer = NULL;
-	int status, index, input_status, there_was_space;
+	int status, isexit, index, input_status, there_was_space;
 	size_t count;
 
 	input_status = isatty(0);
@@ -15,27 +15,36 @@ int main(void)
 		if (input_status == 1)
 			printf("$ ");
 		status = getline(&buffer, &count, stdin);
+		strtok(buffer, "\n");
+		isexit = strcmp(buffer, "exit");
+		if (isexit == 0)
+		{
+			free(buffer);
+			exit(EXIT_SUCCESS);
+		}
 		if (status == EOF)
 		{
 			free(buffer);
-			break;
+			exit(EXIT_SUCCESS);
 		}
-		for (index = 0; *(buffer + index) != '\n'; index++)
+		else
 		{
-			if (*(buffer + index) == ' ' || *(buffer + index) == '\n')
-				there_was_space = 1;
-			else
-				there_was_space = 0;
-			if (there_was_space == 0)
+			for (index = 0; *(buffer + index) != '\n'; index++)
 			{
-				strtok(buffer,  "\n");
-				if (*(buffer + index) == '/' || *(buffer + index) == '.')
-				{
-					path_handle(buffer);
-				}
+				if (*(buffer + index) == ' ' || *(buffer + index) == '\n')
+					there_was_space = 1;
 				else
-					command_handle(buffer);
-				break;
+					there_was_space = 0;
+				if (there_was_space == 0)
+				{
+					if (*(buffer + index) == '/' || *(buffer + index) == '.')
+					{
+						path_handle(buffer);
+					}
+					else
+						command_handle(buffer);
+					break;
+				}
 			}
 		}
 		if (input_status == 0)
